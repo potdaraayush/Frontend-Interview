f39e3bbcbb6813a141df943c4b5519b71853280e
potdaraayush <potdaraayush2023@gmail.com>
2026-01-17 14:38:46 +0530

    tailwind setup successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { BlogList } from "./components/BlogList";
 
+
 function App() {
   return (
+    <>
     <div>
       <BlogList />
     </div>
+    <div className="bg-blue-500 text-white p-4">
+      tailwind test
+    </div>
+    </>
   );
 }
 
